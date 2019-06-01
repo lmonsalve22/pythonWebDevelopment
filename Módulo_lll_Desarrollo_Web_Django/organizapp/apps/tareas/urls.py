d80546405a6e354ddf85f168a9099a698745e2f5
kubos777 <chd.jl@hotmail.com>
2019-06-01 12:36:01 -0500

    Añado login

--- Módulo_lll_Desarrollo_Web_Django/organizapp/apps/tareas/urls.py
+++ Módulo_lll_Desarrollo_Web_Django/organizapp/apps/tareas/urls.py
@@ -1,7 +1,13 @@
 from django.urls import include,path
+from django.contrib.auth import views as auth_views
+
 from . import views
+
 urlpatterns = [
     path('usuarios/',include('apps.usuarios.urls')),
     path('',views.inicio,name='inicio'),
     path('agregar/',views.agregar_tarea,name="agregar_tarea"),
-]
+    path('listar/',views.listar_tarea,name="listar_tarea"),
+    path('login/',views.mi_login,name="mi_login"),
+    path('registrate/',views.registrate,name="registrate"),
+] 
--- Módulo_lll_Desarrollo_Web_Django/organizapp/apps/tareas/views.py
+++ Módulo_lll_Desarrollo_Web_Django/organizapp/apps/tareas/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
 from .forms import FormHomework
+from .models import Homework
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
 # Create your views here.
 
 def inicio(request):
@@ -16,3 +20,30 @@ def agregar_tarea(request):
 		form = FormHomework()
 
 	return render(request,'tareas/agregar_tarea.html',{'form':form})
+
+def listar_tarea(request):
+	tareas = Homework.objects.all(); 
+	return render(request,'tareas/listar_tarea.html',{'tareas':tareas})
+
+def mi_login(request):
+
+	if request.method == 'POST':
+		form = AuthenticationForm(request.POST)
+		username = request.POST['username']
+		password = request.POST['password']
+
+		user = authenticate(username = username,password=password)
+		if user is not None:
+			if user.is_active:
+				login(request,user)
+				return redirect('inicio')
+		else:
+			messages.error(request,'Nombre de usuario o contraseña incorrectos')
+			return redirect('login')
+	else:
+		form = AuthenticationForm()
+	return render(request,'registration/login.html',{'form':form})
+
+
+def registrate(request):
+	pass
